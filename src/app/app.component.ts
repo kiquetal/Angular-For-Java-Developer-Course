3732f80a3864c71df22dab210470475856eeadbe
Enrique M. Talavera <kiquetal@gmail.com>
2021-04-24 23:13:15 -0400

    End section 9 using Input and ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-For-Java';
+
+  @ViewChild('footer',{static:true})
+  footerComponent: FooterComponent;
+  updateLastAccessed() {
+    this.footerComponent.lastAccessed = new Date().toISOString();
+  }
+
+  startTime:string = "";
+
+  ngOnInit(): void {
+
+    this.startTime = new Date().toISOString();
+  }
+
+
 }
